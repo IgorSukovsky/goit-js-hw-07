b54502ae2d23032082a3e24896fb2ea0f681b7c9
IgorSukovsky <Sukovsky70@gmail.com>
2021-04-28 22:04:15 +0300

    сделал 3 задания

---
+++ js/task-03.js
@@ -0,0 +1,35 @@
+const images = [
+  {
+    url:
+      "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
+
+const ulEl = document.querySelector("ul#gallery");
+ulEl.style.display = "flex";
+ulEl.style.alignItems = "flex-start";
+ulEl.style.justifyContent = "space-around";
+
+const createListItem = images.map((image) => {
+  const liEl = document.createElement("li");
+  liEl.style.listStyle = "none";
+  liEl.insertAdjacentHTML(
+    "afterbegin",
+    `<img src= '${image.url}' alt='${image.alt}' width='500px' '
+height = 300'>`
+  );
+  return liEl;
+});
+
+ulEl.append(...createListItem);
